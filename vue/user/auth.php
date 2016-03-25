3b8829992577cd76b47d709c1516feb7b26e084a
Red-Phoenixx <theom2@orange.fr>
2016-03-25 01:36:03 +0100

    New : Dossier User/auth

---
+++ vue/user/auth.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Authentification</title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <h2>Connexion</h2>
+    <form method="post">
+        <label for="pseudo">Pseudo</label>
+        <br />
+        <input type="text" id="pseudo" name="pseudo" autocomplete="off" />
+        <br />
+        <label for="pass">Mot de passe</label>
+        <br />
+        <input type="password" id="pass" name="pass" autocomplete="off" />
+        <br />
+        <input type="submit" id="submit" value="Connexion" />
+    </form>
+    <a href="">Mot de passe oublié ?</a>
+    <!-- Lien à compléter -->
+</body>
+
+</html>
