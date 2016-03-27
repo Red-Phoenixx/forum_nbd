c569c16ec071d5ca9f9b363994a807cf6f5d1ba6
Red-Phoenixx <theom2@orange.fr>
2016-03-28 00:28:24 +0200

    New : Authentification (in progress)

--- vue/user/auth.php
+++ vue/user/auth.php
@@ -4,23 +4,32 @@
 <head>
     <title>Authentification</title>
     <meta charset="utf-8" />
+    <link rel="stylesheet" href="../all.css" />
+    <link rel="stylesheet" href="style.css" />
 </head>
 
 <body>
-    <h2>Connexion</h2>
-    <form method="post">
-        <label for="pseudo">Pseudo</label>
-        <br />
-        <input type="text" id="pseudo" name="pseudo" autocomplete="off" />
-        <br />
-        <label for="pass">Mot de passe</label>
-        <br />
-        <input type="password" id="pass" name="pass" autocomplete="off" />
-        <br />
-        <input type="submit" id="submit" value="Connexion" />
-    </form>
-    <a href="">Mot de passe oublié ?</a>
-    <!-- Lien à compléter -->
+    <div id="bloc_page">
+        <div id="formulaire">
+            <h2>Connexion</h2>
+            <form method="post">
+                <label for="pseudo">Pseudo</label>
+                <br />
+                <input type="text" id="pseudo" name="pseudo" autocomplete="off" required/>
+                <br />
+                <label for="pass">Mot de passe</label>
+                <br />
+                <input type="password" id="pass" name="pass" autocomplete="off" required/>
+                <br />
+                <input type="checkbox" id="check_pseudo" name="check_pseudo" />
+                <label for="check_pseudo">Se souvenir de moi</label>
+                <br />
+                <input type="submit" id="submit" value="Connexion" />
+                <br />
+                <a href="recup_password.php">Mot de passe oublié ?</a>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
